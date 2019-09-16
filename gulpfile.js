cec3b633150b777f6d58add92e5662a7e4ad5d3a
Nicolas Giraud <n.giraud@groupeonepoint.com>
2019-09-16 22:34:51 +0200

    abstract

---
+++ gulpfile.js
@@ -0,0 +1,151 @@
+'use strict';
+
+/** Imports Gulp */
+const gulp = require('gulp');
+const del = require('del');
+const path = require('path');
+const fs = require('fs');
+const browserSync = require('browser-sync').create();
+const $ = require('gulp-load-plugins')();
+var exec = require('gulp-exec');
+
+/** Imports Reveal */
+const map = require('map-stream');
+const asciidoctor = require('asciidoctor.js')();
+require('asciidoctor-reveal.js');
+
+/** Définition des constantes */
+
+// Dossier des sources à builder
+const srcDir = 'prez';
+// Dossier de sortie du build
+const outDir = 'docs';
+// Dossier racine des presentations au runtime (=path d'accès dans l'url. ex: http://..../prez)
+let runtimePrezDir = '/voiture-elec-velo-appt-slides/';
+let cdn = 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0';
+// Dossier de sortie du build des présentations
+const prezOutDir = `${outDir}`;
+
+// Constantes des extensions à prendre en compte pour les différents items du build
+const adocIndexFiles = [`${srcDir}/**/index.adoc`, `${srcDir}/**/index-*.adoc`];
+const adocWatchExtensions = [`${srcDir}/**/*.adoc`];
+const mediasExtensions = [`${srcDir}/**/*.{svg,png,jpg,gif,webm}`];
+const mermaidWatchExtensions = [`${srcDir}/**/*.mmd`];
+const cssExtensions = [`${srcDir}/**/*.css`];
+const jsExtensions = [`${srcDir}/**/*.js`];
+const themesExtensions = [`themes/**/*.*`];
+const pagesExtensions = [`pages/**/*.*`];
+
+gulp.task('convert', () =>
+  gulp.src(adocIndexFiles)
+    .pipe(convertAdocToHtml())
+    .pipe($.extReplace('.html'))
+    .pipe(
+      $.tap((file) => {
+        // Utilisé pour ajouter un js custom au besoin 
+        const customJsPath = path.join(path.dirname(file.path), 'js', 'custom.js');
+        const hasCustomJs = fs.existsSync(customJsPath);
+        if (hasCustomJs) {
+          console.log('Ajout du fichier js custom');
+          const newFile = file.contents.toString();
+          const newContents = newFile.replace('</body>', '<script src="./js/custom.js"></script></body>');
+          file.contents = new Buffer(newContents);
+        }
+
+        return file;
+      }))
+    .pipe(gulp.dest(prezOutDir))
+);
+
+gulp.task('copy-and-generate-mermaid-png', () =>
+  gulp.src(mermaidWatchExtensions)
+    .pipe(gulp.dest(prezOutDir))
+    .pipe(exec('mmdc -i <%= file.path %> -o <%= file.path.replace("mmd", options.ext) %>', { ext: 'png' }))
+);
+
+gulp.task('dependencies', () =>
+  gulp.src('node_modules/reveal.js/{css,js,lib,plugin}/**/*.*')
+    .pipe(gulp.dest(`${prezOutDir}/node_modules/reveal.js`))
+);
+
+gulp.task('copy-medias', () =>
+  gulp.src(mediasExtensions).pipe(gulp.dest(prezOutDir))
+);
+
+gulp.task('copy-css', () =>
+  gulp.src(cssExtensions).pipe(gulp.dest(prezOutDir))
+);
+
+gulp.task('copy-js', () =>
+  gulp.src(jsExtensions).pipe(gulp.dest(prezOutDir))
+);
+
+gulp.task('copy-themes', () =>
+    gulp.src(themesExtensions).pipe(gulp.dest(`${outDir}/themes/`))
+);
+
+gulp.task('copy-pages', () =>
+    gulp.src(pagesExtensions).pipe(gulp.dest(`${outDir}/`))
+);
+
+gulp.task('serveAndWatch', () => {
+  browserSync.init({
+    server: {
+      baseDir: `./${outDir}/`
+    },
+    directory: true,
+    notify: false,
+    port: 3000
+  });
+
+  gulp.watch(adocWatchExtensions, () => $.sequence('convert', browserSync.reload));
+  gulp.watch(mediasExtensions, () => $.sequence('copy-medias', browserSync.reload));
+  gulp.watch(cssExtensions, () => $.sequence('copy-css', browserSync.reload));
+  gulp.watch(jsExtensions, () => $.sequence('copy-js', browserSync.reload));
+  gulp.watch(mermaidWatchExtensions, () => $.sequence('copy-and-generate-mermaid-png', browserSync.reload));
+});
+
+
+gulp.task('clean', () => del(outDir, { dot: true }));
+
+
+// Build production files, the default task
+gulp.task('default', cb =>
+  $.sequence(
+    'clean',
+    'convert',
+    ['dependencies', 'copy-css', 'copy-js', 'copy-medias', 'copy-themes', 'copy-pages', 'copy-and-generate-mermaid-png'],
+    cb
+  )
+);
+
+// Build dev files
+gulp.task('serve', cb => {
+    runtimePrezDir = '/';
+    cdn = '/node_modules/reveal.js';
+      $.sequence('default', 'serveAndWatch', cb)
+    }
+);
+
+
+function convertAdocToHtml() {
+
+  const attributes = {
+      'revealjsdir': `${cdn}@`,
+      'runtimePrezDir': `${runtimePrezDir}`
+  };
+  const options = {
+    safe: 'safe',
+    backend: 'revealjs',
+    attributes: attributes,
+    to_file: false,
+    header_footer: true
+  };
+
+  return map((file, next) => {
+    console.log(`Compilation en html de ${file.path}`);
+    const newContent = asciidoctor.convertFile(file.path, options);
+    file.contents = new Buffer(newContent);
+    next(null, file);
+  });
+};
---
+++ pages/README.md
@@ -0,0 +1,60 @@
+# Comment j'ai fini par déployer à 2h du matin une application sur AWS pour piloter des voitures électriques avec un vélo d'appartement. (In progress)
+
+## Abstract
+
+```
+- J'ai une super idée, mais je vais avoir besoin d'aide, parce que moi et le code...
+ha ?
+- Je voudrais faire tourner des voitures électriques sur un circuit en faisant pédaler des gens pendant 24h
+...
+- Tu vois comment faire ?
+- Alors... c'est pas dur... Il suffit de deux Arduino, d'un Raspberry, quelques capteurs et c'est parti...
+```
+
+Et voilà à peu près comment en 2 minutes j'ai été convaincu de suivre un hurluberlu dans un projet franchement rigolo mêlant de nombreuses technos.
+
+Et cerise sur le gâteau, les aléas techniques m'ont conduit jusqu'à déployer la solution sur AWS depuis une salle des fêtes en rase campagne un jour de tempête avec pour seule connexion mon téléphone...
+
+## Détails
+
+### Plan
+
+* Il était une fois... (4 min)
+	* L'idée - Mise en contexte et description des cas d'usage
+	* Le projet - Présentation des différents composants imaginés au démarrage
+* Réalisations, écueils, et dénouements ! (8 min)
+	* Vélos et voitures
+		* Raspberry vs Arduino - Suite à quelques tests, le gagnant est : Arduino !
+		* Les joies des shields - Quelques déboires électroniques et un POC qui fonctionne
+	* Compte tour et restitutions
+		* Le retour du raspberry ! - Usage de python pour l'appel d'un service REST
+		* Un peu de dev - Implémentation d'un back springboot, et d'un front Angular
+	* Le jour J, alias MEP (8 min)
+		* Le fil rouge... - Intégrer les composants le jour J : mauvaise idée !
+		* Et si rien ne se passait comme prévu...
+			* Ça marchait sur mon poste - Comment un Firewall peut vite devenir un enfer !
+			* Et je met ça où ? - Présentation du déploiement sur AWS (pourquoi et comment)
+	* Défi relevé ? (5 min)
+		* Bilan du projet - En quelques chiffres
+		* Le dev universel - Ouverture sur l'apport de la pluralité
+		* Nous pouvons tous contribuer... - de l'Open source à l'action caritative
+		
+### Compléments
+
+Quelques Tweets sur l'événement : https://twitter.com/nicgiro/status/1071219558613221376
+
+### Resources
+
+[Les slides (In progress)]( ./aws-2019/#/)
+
+## Références 
+
+* Technical Knowledge Sharing (2018-2019) : Mise en place et animation hebdomadaire autour des technos/méthodes du projet (30 min)
+* GDG Tours : [L'adoc : du néant à la galaxie Antora](https://www.meetup.com/fr-FR/GDG-Tours/events/259980415/) (30 pers.)
+* Breizhcamp 2019 : [L'adoc : du néant à la galaxie Antora](https://www.youtube.com/watch?v=67QMncs1wvw) (100 pers.)
+* Nantes DevOps : [L'adoc : du néant à la galaxie Antora](https://www.meetup.com/fr-FR/Nantes-DevOps/events/259469585/) (20 pers.)
+* Présentation technique interne (Juin 2018): Et si l'adoc était accessible à tous ? (30 pers.)
+* Nantes Jug 2017 : [L'aventure #piswarm: Gamifier notre outillage traditionnel pour mettre en avant des concepts innovants.](http://nantesjug.org/#/events/2017_12_14) (50 pers.)
+* Software Crafters Nantes 2016 : [DevOps : Concept à la mode ou réalité ?](https://www.meetup.com/fr-FR/nantes-software-crafters-Nantes/events/235435543/) (30 pers.)
+
+
